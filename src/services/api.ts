09e318cbd3a218dc12c44c12713b55fbd798d946
Narendiran Kesavan <narendiran0206@gmail.com>
2025-05-03 11:27:52 +0530

    url fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { Customer } from '../types';
 
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = '';
+await axios.get('/api/customers');
 
 export const fetchCustomers = async (): Promise<Customer[]> => {
   const res = await axios.get(`${BASE_URL}/customers`);
