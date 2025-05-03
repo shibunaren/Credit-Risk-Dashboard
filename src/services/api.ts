f8c29068d6e1404299050d7f68c5f59fb865dcc5
Narendiran Kesavan <narendiran0206@gmail.com>
2025-05-03 11:52:41 +0530

    path fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Customer } from '../types';
 
 const BASE_URL = '';
-await axios.get('/api/customers');
+await axios.get('/data/customers');
 
 export const fetchCustomers = async (): Promise<Customer[]> => {
   const res = await axios.get(`${BASE_URL}/customers`);
