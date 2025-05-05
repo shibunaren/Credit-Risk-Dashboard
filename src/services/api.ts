35ad60b9bb834f4367767e6a754388da4de8bd4c
Narendiran Kesavan <narendiran0206@gmail.com>
2025-05-05 08:21:04 +0530

    update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Customer } from '../types';
 
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://credit-risk-dashboard-2usf.onrender.com';
 
 export const fetchCustomers = async (): Promise<Customer[]> => {
   const res = await axios.get(`${BASE_URL}/customers`);
